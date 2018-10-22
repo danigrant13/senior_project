4aef938b6a3fc81a64dd7d86151d3b4c02cbc80a
Dani Grant <marrissa.d.grant@gmail.com>
2018-10-22 12:18:10 -0600

    https://github.com/danigrant13/senior_project

--- modules/demoTrial.py
+++ modules/demoTrial.py
@@ -6,32 +6,34 @@ Welcome to the experiment!
 """
 
 intro = """
-               There are two parts to this experiment:
+There are two tasks in this experiment: Task #1 & Task #2.
 
-                                    Game #1
-                                          &
-                                    Game #2
+You will see a series of faces in these tasks. These faces \
+are previous participants who currently attend CU Boulder and \
+had a picture taken during their session like you just did.
 
-You will see a series of faces in this game. These faces are previous participants who currently attend CU Boulder. 
-Based on your results, you may be asked in the future to be paired with these participants in future studies.
+In Task #2, you will be paired up with four of these previous participants \
+and will have the chance to make decisions with real money that can impact \
+yourself and them.
 """
 
-beginWithGame1 = "First, we will describe Game #1"
+beginWithGame1 = "First, we will describe Task #1"
+pressSpace_task1 = "Press the SPACE key to begin instructions for Task #1."
 
-pressSpace = "Press the SPACE key to continue reading the instructions."
+pressSpace = "Press the SPACE key to continue."
 
 summaryText = """
-In Game #1, you will be presented with a steady stream of letters at the center of the screen \
+In Task #1, you will be presented with a steady stream of letters at the center of the screen \
 surrounded by 4 photos (of previous participants) which will periodically rotate 180 degrees and \
 sometimes change to a mirror image.{}
      The basic layout of the trial will look like this:{}
 """.format('\n\n', '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
 
-instructions1Text = 'You have two tasks during this experiment:'
+instructions1Text = 'You have two tasks during this part of the experiment:'
 
 instructions2Text = """
 1. Press the SHIFT key when the letter at the center of the screen is a VOWEL.
-       (i.e. press SHIFT when the letter 'a' appears, but not the letter 'f')
+       (e.g., press SHIFT when the letter 'a' appears, but not the letter 'f')
 
 2. Each time an image rotates, there is a small chance that it will change to its mirror image. 
     At the end of the experimental trial, you will be asked how many image changes there were 
@@ -45,7 +47,7 @@ time an photo rotates.
 """
 
 instructions3Alt = """\
-To rotate and change the image again, press the BACK key.
+To rotate and change the image again, press the BACKSPACE key.
 
 Can you find what is changing in the picture above each time the image rotates?
 
@@ -65,7 +67,7 @@ You must pay close attention to the images to successfully detect them!
 
                 There are three more things you should know:
 
-1. Not every trial of the experiment will have an image change, some will 
+1. Not every trial of task #1 will have an image change, some will 
     have no image changes, and some will have multiple image changes.
 
 2. when an image DOES change, it will only ever do so while it is rotating. 
@@ -90,10 +92,12 @@ def __welcomeAndSummary(win):
     pressSpaceStim.draw()
     win.flip()
     proceedOrQuit(win)
+    
+    pressSpaceStim_task1 = visual.TextStim(win, pressSpace_task1, height=0.06, pos=[0, -0.8], wrapWidth=1.7)
 
     beginStim = visual.TextStim(win, beginWithGame1, wrapWidth=1.7, height=0.10, bold=True)
     beginStim.draw()
-    pressSpaceStim.draw()
+    pressSpaceStim_task1.draw()
     win.flip()
     proceedOrQuit(win)
 
@@ -114,8 +118,10 @@ def screenIntro(win, textStim):
 
 # instruction screen 4
 def screen4(win, textStim):
+    pressSpaceStim = visual.TextStim(win, pressSpace, height=0.06, pos=[0, -0.8], wrapWidth=1.7)
     textStim.setText(screen4Text)
     textStim.draw()
+    pressSpaceStim.draw()
     win.flip()
 
 def drawInstructions(win, instructions, demoPic, count=3):
@@ -133,6 +139,7 @@ def __rotateDemoImage(win, demoPicStim, rotateSpeed, demoPicChange, instructions
         drawInstructions(win, instructions, demoPicStim)
 
 def run(context):
+    pressSpaceStim = visual.TextStim(win, pressSpace, height=0.06, pos=[0, -0.8], wrapWidth=1.7)
     win = context['window']
     demoPicA = 'demo_a.jpg'
     demoPicB = 'demo_b.jpg'
@@ -171,6 +178,7 @@ def run(context):
     # introduction screen 3
     instructions[2].setText(instructions3Answer)
     drawInstructions(win, instructions, demoPic)
+    pressSpaceStim.draw()
     proceedOrQuit(win)
 
     gettingInput = True
