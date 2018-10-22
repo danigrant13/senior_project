7e2d910dbba1ee2c425bee503e3a0120cf003023
Dani Grant <marrissa.d.grant@gmail.com>
2018-10-22 12:36:47 -0600

    https://github.com/danigrant13/senior_project

--- modules/trustGameInstructions2.py
+++ modules/trustGameInstructions2.py
@@ -7,68 +7,56 @@ grey = (217, 217, 217)
 red = (255, 0, 0)
 green = (169, 209, 142)
 
-age1introGame = """ \
-Now, we will describe Game #2 \
+introTask = """ \
+Now, we will describe Task #2. \
 """
+pressSpace_task2 = "Press the SPACE key to continue reading the instructions for Task #2."
 
-pressSpace = "Press the SPACE key to continue reading the instructions."
+pressSpace = "Press the SPACE key to continue."
+
+pressSpace_fin = "Press the SPACE key to finish reading the instructions."
 
 summaryText = """
-In Game #2, you will again be presented with a screen surrounded by four photos of previous participants. \
-They will be labeled A, B, C, and D. In addition, you will be given a letter designation, 'E', ]
-to indicate you are a player in this game.{}
+In Task #2, you will again be presented with a screen surrounded by four photos of previous participants. \
+They will be labeled A, B, C, and D.{}
      The basic layout of the trial will look like this:{}
 """.format('\n\n', '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
 
 summaryText2 = """
-In Game #2, each player (including you) will be allotted $5.00 each in their personal bank.{}
+In Task #2, each participant (including you) will be allotted $5.00 each in their personal bank.{}
      The basic layout of the trial will look like this:{}
 """.format('\n\n', '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
 
 title = """ \
-There are two roles in this game:
+There are two roles in this task:
 First Mover & Second Mover"""
 
 gameRoles = """
-The First Mover is randomly selected among all the participants
+You will be the First Mover.
 
-The first mover chooses which of the other four participants they would like to play the trust game with
+You wil choose how much money, if any, to send to the other participants (Second Mover).\
+You can give as little as $0.00 or as much as $5.00 (in $0.25 increments), to the other participants in total.
 
-the first mover is given the opportunity to give as little as $0.00 or as much as $5.00, or any amount in between (in $0.25 increments), so their participant of choice
-
-The money given to the chosen participant will be multiplied by three and placed in the chosen participant's bank
-"""
-
-gameRoles2 = """ 
-One the first mover has finished deciding how much to allot \
-to the chosen participant, the first mover will be given the \
-chance to choose a different participant to play the trust game with
-
-This cycle will continue until the first mover is satisfied with the \
-choices he has made about money allotted (or not) during the first round
-"""
-
-gameRoles3 = """ 
-After the First Mover's turn is over, the Second Mover will be randomly \
-chosen from one of the four other participants
-
-The Second Mover will be given the opportunity to return as \
-little as $0.00 and as much as they received, or any amount in \
-between (in $0.25 increments), back to the First Mover
-
-The Second mover CANNOT give any of the original $5.00 \
-placed in their account at the beginning of the game
+Any money you send will be multiplied by three and placed in the chosen participant's account. In a follow-up \
+session, they will be given the chance to send some, none, or all of that money back to you.
 """
 
 summaryText3 = """
-During the game, a RED BOX will indicate First Mover, \
-while a GREEN BOX will indicate Second Mover.{}
+During the game, a RED BOX will indicate current Mover, \
+while a GREEN BOX will indicate receiver.{}
 """.format('\n\n', '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
 
 def run(context):
     win = context['window']
     pressSpaceStim = visual.TextStim(win, pressSpace, height=0.06, pos=[0, -0.8], wrapWidth=1.7)
+    pressSpaceStim_task2 = visual.TextStim(win, pressSpace_task2, height=0.06, pos=[0, -0.8], wrapWidth=1.7)
+    pressSpaceStim_fin = visual.TextStim(win, pressSpace_fin, height=0.06, pos=[0, -0.8], wrapWidth=1.7)
+
     pages = [
+        [
+            pressSpaceStim_task2,
+            visual.TextStim(win, introTask, pos=[0, 0],height=0.1,wrapWidth=1.7),
+        ],
         [
             pressSpaceStim,
             visual.TextStim(win, summaryText, pos=[0, 0],height=0.07,wrapWidth=1.7),
@@ -80,19 +68,9 @@ def run(context):
             visual.ImageStim(win, image='trustDemo2.jpg', mask=None, units='norm', pos=[0, -0.2], size=[0.88, 1.0])
         ],
         [
-            pressSpaceStim,
+            pressSpaceStim_fin,
             visual.TextStim(win, title, pos=[0, 0.8],height=0.1,wrapWidth=1.7),
             visual.TextStim(win, gameRoles, pos=[0, 0],height=0.07,wrapWidth=1.7)
-        ],
-        [
-            pressSpaceStim,
-            visual.TextStim(win, title, pos=[0, 0.8],height=0.1,wrapWidth=1.7),
-            visual.TextStim(win, gameRoles2, pos=[0, 0],height=0.07,wrapWidth=1.7)
-        ],
-        [
-            pressSpaceStim,
-            visual.TextStim(win, title, pos=[0, 0.8],height=0.1,wrapWidth=1.7),
-            visual.TextStim(win, gameRoles3, pos=[0, 0],height=0.07,wrapWidth=1.7)
         ],
         [
             pressSpaceStim,
